94b40b02520b200554b033269e6863745b6a419f
Tiansi Dong <tdong@uni-bonn.de>
2016-12-01 15:00:26 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# sentence to parts
+A Python module to decompose a sentence into parts
+
+# Quick start
+```
+$ git clone https://github.com/Himmelstein/snt2parts
+$ cd snt2parts
+snt2parts $ make init
+```
+
+# Run test
+```
+snt2parts $ make test
+```
+
+# Generate documentation
+```
+snt2parts $ ./make_docu
+```
+Documentation is located at docs/html/
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='snt2parts',
+      version='0.1',
+      description='decompose sentence into its parts, g-gram, etc.',
+      url='http://github.com/HimmelStein/snt2parts',
+      author='Tiansi Dong',
+      author_email='tian1shi2@gmail.com',
+      license='MIT',
+      packages=['snt2parts'],
+      zip_safe=False)
